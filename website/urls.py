d609dab4e5231e3d1bd434d6be39b47037583b76
Shreyas Chand <shreyas.chand@gmail.com>
2013-04-12 14:46:25 -0700

    created urls for static pages

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,8 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     url(r'^about/$', direct_to_template, {'template' : 'website/about.html'}),
+    url(r'^browse/$', direct_to_template, {'template' : 'website/browse.html'}),
+    url(r'^login/$', direct_to_template, {'template' : 'website/login.html'}),
+    url(r'^forgot/$', direct_to_template, {'template' : 'website/forgot.html'}),
+    url(r'^signup/$', direct_to_template, {'template' : 'website/signup.html'}),
 )
