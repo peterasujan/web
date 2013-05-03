2d7befe1a4963d6b0283ed3b2c19fca2489d37a0
Shreyas Chand <shreyas.chand@gmail.com>
2013-05-03 15:25:23 -0700

    added some more mvle stuff

--- mvle/views.py
+++ mvle/views.py
@@ -1,1 +1,14 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.utils import simplejson
+from django.shortcuts import render_to_response
+
+def process_mvle(request):
+    status = {'status': 'failure'}
+    if request.method == POST:
+
+        return render_to_response('mvle/single-mc.html', {'data': request.body})
+
+    else:
+        status['message'] = "Bad HTTP request type: Use POST instead"
+    return HttpResponse(simplejson.dumps(status), mimetype="application/json")
