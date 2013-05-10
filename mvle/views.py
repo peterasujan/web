c85a3bdf73cc2df6fa5831d0ed0f69ac54d725e8
Shreyas Chand <shreyas.chand@gmail.com>
2013-05-09 18:18:48 -0700

    fixed some bugs in mvle. feedback still not showing up.

--- mvle/views.py
+++ mvle/views.py
@@ -8,8 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 def process_mvle(request):
     status = {'status': 'failure'}
     if request.method == 'POST':
-        return render_to_response('mvle/single-mc.html', {'data': request.body})
+        return render_to_response('mvle/single-mc.html', {'data': request.POST['data']})
     else:
-        status['message'] = "Bad HTTP request type: Use POST instead"
-    return render_to_response('mvle/single-mc.html', {'data': request.GET['data']})
-    return HttpResponse(simplejson.dumps(status), mimetype="application/json")
+        #status['message'] = "Bad HTTP request type: Use POST instead"
+        return render_to_response('mvle/single-mc.html', {'data': request.GET['data']})
+    #return HttpResponse(simplejson.dumps(status), mimetype="application/json")
