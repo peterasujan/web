a9cc304e2580802601ab21b5aadbfd96621fa802
Shreyas Chand <shreyas.chand@gmail.com>
2013-05-11 19:13:39 -0700

    added admin view for log viewing

---
+++ log/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from log.models import *
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'verb', 'time')
+
+
+admin.site.register(LogAdmin, ActivityLog)
